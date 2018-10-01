96a32315fdf8841df7443e6fb267f678407e350c
pwipwi <pasc.winter@gmail.com>
2018-10-01 14:16:34 +0200

    Commit
    Commit

---
+++ test.php
@@ -0,0 +1,150 @@
+<?php 
+
+echo " 
+
+1091;Ownership culture;fr-FR;;learning_objects/3441C34D-9D23-F6BA-A055-54067AAB85B5.pdf
+1092;Manuel de Management Siemens SAS;fr-FR;;learning_objects/05C60B91-6D97-9378-FFC4-C20599CE2E4E.pdf
+993;Aide à la fixation d'objectifs - WHAT - HOW_PMP 2017;fr-FR;;learning_objects/A418E1A8-AB50-9B13-BCCB-230910230818.pdf
+485;Le rôle du manager;fr-FR;;learning_objects/6731D101-B093-B52B-4C1F-04B588887548.pdf
+487;Corporate Top talents;fr-FR;;learning_objects/3B814E42-E453-B47F-2255-12CFD521586E.pdf
+488;Charte du courriel;fr-FR;;learning_objects/D21112B2-51B6-E077-FD3C-366971BDAA9C.pdf
+489;Définition des Degrés d'Implémentation de nos projets;fr-FR;;learning_objects/55006FC4-7201-C3E3-37CB-53E3D3BD5797.pdf
+490;Guidelines du mentoring;fr-FR;;learning_objects/5DC19D48-7B9E-4904-629E-5972853559F0.pdf
+491;Guidelines recrutement;fr-FR;;learning_objects/0F260A2D-9EB1-9948-5F0C-AB1A8B6F1440.pdf
+513;La Charte de l'Ambassadeur-écoles;fr-FR;NULL;learning_objects/6979E8E8-28E0-7E50-E4CF-A343F50BAC08.pdf
+514;Liste Ambassadeurs-écoles 2014;fr-FR;NULL;learning_objects/55797376-D295-CD35-CA66-BE95D10A9E1F.xls
+727;Listes des salons 2014-2015;fr-FR;;learning_objects/3BB300F4-6D97-A3E4-3BBC-EE74793592E8.pdf
+516;Le rôle du manager;fr-FR;;learning_objects/A3917D3F-CB9A-6CAD-EE58-09CDD1D597C5.pdf
+523;Siemens Corporate Forms 2014;fr-FR;;learning_objects/90EA3BDA-AFC2-1D1B-21AE-53F04FF43626.pdf
+524;Charte Shaping Our Future;fr-FR;;learning_objects/1CF2508E-86C4-518C-B9A6-874E281D9B89.pdf
+563;CK_CaseStudy_miparcours;fr-FR;NULL;learning_objects/C01E1DE8-9425-B33B-77BE-AACB462D0034.docx
+584;Time 2 Produce / Level 1 / Etude de cas / fr;fr-FR;NULL;learning_objects/B24DFBEA-3C1B-0CB6-C846-857E09CECB21.pdf
+585;Time 2 Produce / Level 1 / Case Study;en-GB;NULL;learning_objects/C1E228DC-5659-431D-BA6C-D07DCC29A872.html
+601;User Guide;en-GB;NULL;learning_objects/15ABDE62-D4B2-AF0E-AE4B-44ABBFF3E709.pdf
+602;User Guide;fr-FR;NULL;learning_objects/0A6686A7-7D9F-8BF8-D2F5-217C63D6463A.pdf
+605;Guide Utilisateur 12.1;fr-FR;NULL;learning_objects/A1CD9D51-5E30-4E17-8A79-FA707295E93B.pdf
+690;INSEAD - Dysfunctional Leadership;fr-FR;NULL;learning_objects/1B423C55-B313-D804-1E27-AD0A9B693C42.pdf
+691;Virtual teams, effective Leadership;fr-FR;NULL;learning_objects/2EDFEDC6-D341-6F88-7845-AD641EC6250F.pdf
+692;Executive summary de la conférence Greenworking;fr-FR;NULL;learning_objects/88A51AB5-AF13-3588-3024-A52B6F06B492.pdf
+698;Présentation Plenière ;fr-FR;;learning_objects/2942A0F0-B1FB-0AAF-3053-0CEB80385ED8.pdf
+700;Mémo Siemens France, messages clés;fr-FR;;learning_objects/3398062B-EDF3-8FBD-FB8A-2A8EB0D1960C.pdf
+728;Fiche mémo SMA - Le CDD;fr-FR;;learning_objects/01F2D452-5BB4-6867-16B7-C679E1D82F2D.pdf
+729;Fiche mémo SMA - La période d'essai;fr-FR;;learning_objects/132CE821-2176-BBDC-55C3-BD4D77DE02AC.pdf
+730;Fiche mémo SMA - Le Compte Personnel de Formation (CPF);fr-FR;;learning_objects/17EE01AA-AE0B-CB43-01F2-72C060A9337C.pdf
+732;Articles manageriaux d’intérêt : La fuite managériale devant la complexité - l'exemple du LEAN management;fr-FR;;learning_objects/F6CF6929-9629-0960-DEF4-6A2855673B14.pdf
+745;Gestion des risques psycho-sociaux;fr-FR;;learning_objects/5C5BBC77-D87C-657F-2237-4436F7D88F6D.pdf
+919;Performance Management Process (PMP) – Les nouveautés 2015;fr-FR;;learning_objects/A301A5E6-382E-3709-6A8F-2801BB9BCCFD.pdf
+935;Vision 2020;fr-FR;;learning_objects/A397E992-43C0-05A2-860B-505FBFBB317E.pdf
+980;Module QEHS - présentation;fr-FR;;learning_objects/61889A66-085C-3C2B-0012-2D5168E21915.pdf
+981;Passeport manager - Module Recrutement Formation Carrières pour mise à disposition apprenants;fr-FR;;learning_objects/E43F08FC-ADD2-1E0B-2206-E9A790F27C6B.pdf
+984;Fiche mémo SMA - La Validation des acquis de l'Expérience - (VAE);fr-FR;;learning_objects/41E1C732-DB67-1B06-BFD9-EA5AC641162F.pdf
+985;Fiche mémo SMA- Le Conseil en Evolution Professionnel - (CEP);fr-FR;;learning_objects/ED224EBC-1BE8-2714-E6D6-FD2D66CCCCA1.pdf
+986;Fiche mémo SMA - Le Congé Individuel de Formation - (CIF);fr-FR;;learning_objects/23230C8A-969D-C576-5A10-16E29AB5D10C.pdf
+987;Fiche mémo SMA - L' Entretien professionnel;fr-FR;;learning_objects/DB6587F1-40B4-7B53-6BE9-505F2C5B3C3B.pdf
+989;Passeport manager - Module Recrutement Formation Carrières;fr-FR;;learning_objects/28E72266-DF7E-5338-E1AD-3CEDA62059E1.pdf
+997;Livemeeting 2016-01 – Entretiens professionnels pour managers;fr-FR;;learning_objects/15EC96A5-9F36-0DB7-4E8A-0D1FC1371849.pdf
+1074;Performance Management Process (PMP- Les nouveautés 2016);fr-FR;;learning_objects/1D05EFA3-4E80-1511-D8B1-CECC37E9AE7B.pdf
+1076;Processus de gestion de l’intérim via Fieldglass;fr-FR;;learning_objects/5FB12A5C-B3E1-17B0-BFA9-7FE0E3F0C116.pdf
+527;Découvrez les programmes certifiants ESSEC ! ;fr-FR;;learning_objects/2132957B-B1C0-D761-0C93-A598F9CC7F36.mp4
+693;The_Clarins_Race_PROD2;fr-FR;;learning_objects/1D4E7039-F1D2-5FB0-C7A6-2C5DBE8582C2.MPG
+694;Clarins_Athenes_Service_to_Customer_h264;fr-FR;;learning_objects/D6284FE9-C960-8455-3CB6-5BA581A5F661.MPG
+695;Video_Luchar_Celulitis_Rebelde_ES;fr-FR;;learning_objects/D94EE2D9-A233-ADE0-2121-D1A01E91EC21.mp4
+696;SANOFI Vidéo ACADEMY WHY V2;fr-FR;;learning_objects/96FDA735-048F-D1B2-892E-659740DDF7A8.mp4
+697;Module A JP COLLIN;fr-FR;;learning_objects/D8584D17-3830-CBDD-747C-6C703B90197C.mp4
+707;DEF_applausi(1);fr-FR;;learning_objects/3A7191D6-7E80-6689-A592-1538D22D88B1.mp4
+918;Ownership Culture;fr-FR;;learning_objects/7C3CCAC9-68BC-92A6-C116-CF6FABE49376.wmv
+936;Ownership Culture;fr-FR;;learning_objects/878BD15E-22AE-5277-A3EE-7A0C36159F3F.wmv
+937;Formula 1 Pit Stops 1950 & Today;fr-FR;;learning_objects/2F40886D-28CA-00E2-9598-F8357DFDC358.mp4
+979;The interviewer - Le recruteur;fr-FR;;learning_objects/BCC74D57-8563-8ED8-3A1D-DDC1B66B4757.mp4
+990;Conférence K. Helmrich - Digitalisation (Saint-Denis, 20 Oct 2015);fr-FR;;learning_objects/0728835F-21E6-5651-70B8-A3A306A60610.mp4
+999;Dialogue avec Joe Kaeser (Saint-Denis, 10 février 2016);fr-FR;;learning_objects/0D17BF2E-761C-258F-AE39-C5DB6FC030DB.mp4
+1093;SMA - Module QEHS - présentation principale v12 _01 02 2017;fr-FR;;learning_objects/3BBE259B-8F14-DDED-6B9B-8139EF405D28.pdf
+1094;Passeport manager 020217 - Module Recrutement pour mise à disposition apprenants sur SMA;fr-FR;;learning_objects/BD6662B4-389C-2567-AF90-E2E3C9C75EAA.pdf
+558;Différences formation - session - apprenant;fr-FR;NULL;learning_objects/9B4AEBB8-7F46-C28C-5B17-D64DF85EC3DA.flv
+559;Understand_difference_between_training_course_session;en-GB;NULL;learning_objects/4B61F1FA-CD62-BA24-5326-0A84C2FDA26C.flv
+560;Recognize the 3 different types of training courses;en-GB;NULL;learning_objects/6C68C67B-7C6E-4849-E0CD-7716CFC01240.flv
+561;Reconnaître les 3 types de parcours;fr-FR;NULL;learning_objects/5CB0DDC0-3F03-4871-9B80-2FE96BA99CE7.flv
+562;Créer et gérer les Groupes;fr-FR;NULL;learning_objects/F232B04D-A3D4-E74B-881C-E133C6053217.flv
+564;Créer un parcours 100% distanciel;fr-FR;NULL;learning_objects/378470BE-30F8-0F16-7818-25F3E44639E9.flv
+565;Créer un parcours de formation multimode;fr-FR;NULL;learning_objects/3CDD46C8-21B3-65F9-29BA-A79F91FF510C.flv
+566;Créer et modifier des classes;fr-FR;NULL;learning_objects/11FE0D9F-C5CC-6173-3967-01D0066618B1.flv
+567;Integrer des documents au catalogue;fr-FR;NULL;learning_objects/CAF63AAF-CF8C-1763-AB19-63131C720AC1.flv
+568;Accéder au reporting détaillé;fr-FR;NULL;learning_objects/A83D0290-A2DE-3CAD-B3CF-02A9DF8C6E3E.flv
+569;Acceder au reporting global;fr-FR;NULL;learning_objects/0E9963A4-9359-821F-FF7C-62DF18672F0A.flv
+570;Creer un questionnaire;fr-FR;NULL;learning_objects/5E58E4CF-6CE9-353F-14A7-5B12934E01D6.flv
+571;Integrer un questionnaire CK prescriptif dans un parcours;fr-FR;NULL;learning_objects/A63C7DBD-0BF4-FC4A-0C7E-8936774CB632.flv
+572;Intégrer un questionnaire sur des contenus tiers dans un parcours;fr-FR;NULL;learning_objects/C692D8A8-F5E4-D32C-45B9-E552E014FF7B.flv
+573;Gérer les discussions;fr-FR;NULL;learning_objects/678AFC01-1098-96CB-CD91-BB2C712DCAA7.flv
+574;Manage discussions;en-GB;NULL;learning_objects/9C95780A-D1EA-21F7-06CC-D28976A47B1A.flv
+575;La Modération des discussions;fr-FR;NULL;learning_objects/C5356D80-B32D-5C48-C9D3-F378A6488A87.flv
+576;Moderate discussions;en-GB;NULL;learning_objects/35F9DE3C-07DA-6AD7-758E-472B3B7C0442.flv
+577;Parametrer un plan d'animation;fr-FR;NULL;learning_objects/3BA41A44-2F1A-9A90-562B-56DD8F8561C5.flv
+578;Create a learner account;en-GB;NULL;learning_objects/D560C243-B2A6-EA2D-2AC1-758C6E8CB88D.flv
+579;Ajouter un apprenant;fr-FR;NULL;learning_objects/30CEB272-6318-7C0D-D74B-ED531D5930BA.flv
+580;Register Learners for a Session;en-GB;NULL;learning_objects/CFED7B54-440B-1C6E-C4A9-19A6E35525E3.flv
+581;Inscrire un apprenant à une session;fr-FR;NULL;learning_objects/D4860743-50EF-C571-6CFE-E4DDE4A8BF28.flv
+582;Personnalisation Fiche Apprenant;fr-FR;NULL;learning_objects/D5F40B8C-6633-4A8C-F39E-57EC81238C12.flv
+586;Rapports depuis la page d'un apprenant;fr-FR;NULL;learning_objects/FD24182E-AE3E-C485-6B43-0F648FEEDD8F.flv
+587;Analyse training performance - Group report overview;en-GB;NULL;learning_objects/E246562F-C66C-F59A-4F15-7406373BBB66.flv
+588;Rapports depuis la fiche d'un groupe;fr-FR;NULL;learning_objects/072F72EB-8D33-EEA5-F8C5-319C88FD4C9A.flv
+540;Siemens Management Academy_Teaser;fr-FR;NULL;learning_objects/E1FF0DF5-6F4A-AD08-01D9-4ACF67EEA504.mp4
+589;Présentation des tableaux de bord;fr-FR;NULL;learning_objects/F5D42861-0EBA-3512-DE91-3D8F02983876.flv
+590;Analyse_training_performance__Dashboard_and_Global_Activity_EN;en-GB;NULL;learning_objects/015C2F37-8ACC-4CA2-6248-36C9715311E3.flv
+591;Analyse training performance - Session report overview;en-GB;NULL;learning_objects/F1915D72-7EFA-2610-E5DD-6DE37221069B.flv
+592;Rapports depuis la page d'une session;fr-FR;NULL;learning_objects/DF42A9A7-C9BA-9882-3A4D-9143AF6A21B1.flv
+593;Comment créer une formation Knowledge Community ?;fr-FR;NULL;learning_objects/9630AF67-3592-3DB9-B5E1-E45186A4BBDE.flv
+594;How to create Knowledge Community training course;en-GB;NULL;learning_objects/AF9398F0-647E-2052-13AC-8B6789421451.flv
+595;Comment gérer les contributions d'une Knowledge community ?;fr-FR;NULL;learning_objects/7581652B-A535-B164-ADBA-C26824D07811.flv
+596;How to manage learner's contributions ?;en-GB;NULL;learning_objects/73494273-B3BB-BFEE-7EB8-8148BF72FC01.flv
+597;Inscriptions et accès à une formation Knowledge Community;fr-FR;NULL;learning_objects/DE52788F-121B-DB04-706E-959EB798DF21.flv
+598;How to manage enrollment to a Knowledge Community training course ?;en-GB;NULL;learning_objects/422B611D-E07A-8492-336A-2BDFCB99B2D0.flv
+599;Comment générer des URL d'auto-connexion ?;fr-FR;NULL;learning_objects/52FDF2FA-E08A-2EFA-5957-8FB272C766BE.flv
+600;How to generate auto-connection URLs ?;en-GB;NULL;learning_objects/CE593B2B-0581-BEEA-4D12-8FD4207A1975.flv
+654;Gerer les metadonnees dun contenu;fr-FR;Gerer les metadonnees dun contenu;learning_objects/28BBC6FE-4500-46AA-8FFE-DEDDE7BDC317.mp4
+655;Creer une formation avec le Wizard;fr-FR;Creer une formation avec le Wizard;learning_objects/042295BC-5803-0466-D418-1ABAEDC94CAF.mp4
+656;Importer des apprenants en lot;fr-FR;Importer des apprenants en lot;learning_objects/5E8E630E-12BC-65AF-E14C-6894CE5C4D93.mp4
+657;Inscrire un apprenant a une session de formation;fr-FR;Inscrire un apprenant a une session de formation;learning_objects/F78424C2-1C4C-5B59-22DF-A25A798FFB61.mp4
+659;Le rapport de pilotage et le rapport individuel par contenu;fr-FR;Le rapport de pilotage et le rapport individuel par contenu;learning_objects/9AFC8A68-5927-C302-B18C-B9B87AF7CEBD.mp4
+660;Les rapports presents sur la fiche dun apprenant;fr-FR;Les rapports presents sur la fiche dun apprenant;learning_objects/318826C3-29F2-60F9-A5D3-7FC362981402.mp4
+661;Localisation et versionning;fr-FR;Localisation et versionning;learning_objects/49FF1290-DB8B-69DC-6C5E-93B6DF15C494.mp4
+662;Personnalisation de la fiche apprenant;fr-FR;Personnalisation de la fiche apprenant;learning_objects/20784531-F6AC-7AAE-1FA5-D2A09C6607A6.flv
+664;A la decouverte des tableaux de bords;fr-FR;A la decouverte des tableaux de bords;learning_objects/0930B751-EFB6-0930-4A0B-EA00C65E69CD.mp4
+665;Ajouter des contenus dans la plateforme;fr-FR;Ajouter des contenus dans la plateforme;learning_objects/B4994E05-A0A7-8A16-A845-1A447FEE1784.mp4
+666;Ajouter des questions a un contenu;fr-FR;Ajouter des questions a un contenu;learning_objects/0DD50501-91E3-FF9D-9567-89237B6FF96D.mp4
+667;Ajouter un apprenant dans la plateforme;fr-FR;Ajouter un apprenant dans la plateforme;learning_objects/C8730CAF-9E6C-7812-F1B0-1E26614B2576.mp4
+668;Assess and Learn;fr-FR;Assess and Learn;learning_objects/B2C9E12E-10E7-06D7-E59F-92A939461C32.mp4
+669;Comment configurer un email dans la plateforme;fr-FR;Comment configurer un email dans la plateforme;learning_objects/D3629455-7BE1-46E9-57A8-AF955E541CE8.mp4
+670;Configurer un email sur objectif;fr-FR;Configurer un email sur objectif;learning_objects/3BDA2449-E23B-3D39-EBEE-B7C2FD012A26.mp4
+671;Configurer une mail JN;fr-FR;Configurer une mail JN;learning_objects/CB160E98-FD79-B829-8D50-F826D07C0058.mp4
+672;Creer et gerer les groupes;fr-FR;Creer et gerer les groupes;learning_objects/382D467A-8164-76E0-D8FD-0879AE416FBC.mp4
+658;Introduction à Time2Produce;fr-FR;Introduction a la plateforme Crossknowledge;learning_objects/7328ED2A-4C06-13D7-1A66-F281A76612A4
+583;Time 2 Produce / Level 1 / Etude de cas / fr;fr-FR;NULL;learning_objects/AB1C1124-3AD8-30C7-311A-58E7D2E30DD1
+556;Introduction à la plateforme LMS Crossknowledge;fr-FR;NULL;learning_objects/445C72AE-1258-B592-5767-21C4E0837589
+557;Introduction to Time2Produce;en-GB;NULL;learning_objects/B17E6369-EEAC-14FA-8517-75EC007C419F
+555;Introduction à la plateforme Crossknowledge (French version);fr-FR;Mohive_TB3815;learning_objects/E3D542E6-7346-23BF-9CAA-FA2DA8F35325
+1111;Stéréotypes de genre;fr-FR;;learning_objects/305F1C7A-0ED0-1613-45F3-58779398184C.pdf
+1113;Pocket-cards;fr-FR;;learning_objects/696BEA58-2989-75D0-47F5-3A7D398E3684.pdf
+1120;Volatile Uncertain Complex Ambiguous - These are the skills leaders need in a VUCA world;fr-FR;;learning_objects/0DB3A6ED-12E1-D3C4-1F8C-7D75234E9A08.mp4
+1124;Own your career;fr-FR;;learning_objects/4982C525-AAF1-D946-E771-5006699F01FF.pdf
+1126;Social Job Sharing by Siemens;fr-FR;;learning_objects/B0D79010-868B-719B-28BF-BF39F8196A70.pptx
+1129;Tutoriel : Créer ses profils sur les réseaux sociaux LinkedIn et Twitter;fr-FR;;learning_objects/0B33937E-5348-271F-213A-8C26BA10EC70.pdf
+1130;Covers LinkedIn;fr-FR;;learning_objects/A9E4A39E-2E8E-AE86-3B16-440BD6148D34.zip
+1131;Guide de préparation à l'évaluation;fr-FR;;learning_objects/4272A27B-EDE6-D93B-B3C0-BA2D5C20CFE1.pptx
+1154;PMP 2017 - présentation managers;fr-FR;;learning_objects/5CC02449-3C19-A8A7-E6E6-F8297E95A654.pdf
+1158;Fiche mémo SMA - Congés spéciaux;fr-FR;;learning_objects/19002BA7-7080-5B45-EF23-7819A2415C75.pdf
+1159;Fiche mémo SMA - Le bilan de compétences;fr-FR;;learning_objects/A31B8B32-6E8F-BCA3-DB1F-3EA32A5B3EBE.pdf
+1160;Fiche mémo SMA - Le rôle des IRP;fr-FR;;learning_objects/634E4E0C-0197-5551-BB87-C4FA146457CF.pdf
+1161;Fiche mémo SMA - Mener un entretien de recadrage;fr-FR;;learning_objects/0228783B-230D-468D-FF68-7EF19DE55617.pdf
+1197;Droit social pour managers;fr-FR;;learning_objects/984EC85F-08FC-3B24-ACFF-CBBED9211713.pdf
+1198;Stéréotype de genre;fr-FR;;learning_objects/F85C1C9B-2467-BE65-E759-98072356C574.pdf
+1199;FDMS_Communication managériale_2;fr-FR;;learning_objects/73D8AE81-0A2D-4FE8-C34B-A9350684F8B0.pdf
+1200;FDMS_Communication managériale_1;fr-FR;;learning_objects/77AA2F77-C226-949D-F1CC-FF3905DB1A1F.pdf
+1201;FDMS_Manager le collectif_M2;fr-FR;;learning_objects/710A64D6-BF59-B4A2-CECB-48317F5567C0.pdf
+1202;FDMS_Manager les individus_M1;fr-FR;;learning_objects/1746AC29-3723-3677-92D0-EC86D2DD7925.pdf
+1203;FDMS_Posture du manager_M0;fr-FR;;learning_objects/EB1D9F6F-E247-39E9-1080-A537398B6AF0.pdf
+1204;Passeport Manager Module QEHS;en-GB;;learning_objects/F033A580-8ABC-6509-6A5F-642A89FF5460.pdf
+1205;FDMS_Module RH;en-GB;;learning_objects/CB3FA01E-ADE4-D83B-D98F-1C25A4860D93.pdf
+1206;Circuit SMA;en-GB;;learning_objects/E9DE511D-0EE4-DCDE-D11F-5C97EE29FE9E.pdf
+" ; 
+
+?>
