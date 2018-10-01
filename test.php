310efe074008d73378e396db35359cb9c77bd07f
pwipwi <pasc.winter@gmail.com>
2018-10-01 14:24:33 +0200

    commit
    commit

--- test.php
+++ test.php
@@ -127,7 +127,19 @@ echo "
 1120;Volatile Uncertain Complex Ambiguous - These are the skills leaders need in a VUCA world;fr-FR;;learning_objects/0DB3A6ED-12E1-D3C4-1F8C-7D75234E9A08.mp4
 1124;Own your career;fr-FR;;learning_objects/4982C525-AAF1-D946-E771-5006699F01FF.pdf
 password=thisisthepassword
------BEGIN DSA PRIVATE KEY-----
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQCqGKukO1De7zhZj6+H0qtjTkVxwTCpvKe4eCZ0FPqri0cb2JZfXJ/DgYSF6vUp
+wmJG8wVQZKjeGcjDOL5UlsuusFncCzWBQ7RKNUSesmQRMSGkVb1/3j+skZ6UtW+5u09lHNsj6tQ5
+1s1SPrCBkedbNf0Tp0GbMJDyR4e9T04ZZwIDAQABAoGAFijko56+qGyN8M0RVyaRAXz++xTqHBLh
+3tx4VgMtrQ+WEgCjhoTwo23KMBAuJGSYnRmoBZM3lMfTKevIkAidPExvYCdm5dYq3XToLkkLv5L2
+pIIVOFMDG+KESnAFV7l2c+cnzRMW0+b6f8mR1CJzZuxVLL6Q02fvLi55/mbSYxECQQDeAw6fiIQX
+GukBI4eMZZt4nscy2o12KyYner3VpoeE+Np2q+Z3pvAMd/aNzQ/W9WaI+NRfcxUJrmfPwIGm63il
+AkEAxCL5HQb2bQr4ByorcMWm/hEP2MZzROV73yF41hPsRC9m66KrheO9HPTJuo3/9s5p+sqGxOlF
+L0NDt4SkosjgGwJAFklyR1uZ/wPJjj611cdBcztlPdqoxssQGnh85BzCj/u3WqBpE2vjvyyvyI5k
+X6zk7S0ljKtt2jny2+00VsBerQJBAJGC1Mg5Oydo5NwD6BiROrPxGo2bpTbu/fhrT8ebHkTz2epl
+U9VQQSQzY1oZMVX8i1m5WUTLPz2yLJIBQVdXqhMCQBGoiuSoSjafUhV7i1cEGpb88h5NBYZzWXGZ
+37sJ5QsW+sJyoNde3xH8vdXhzU7eT82D6X/scw9RZz+/6rCJ4p0=
+-----END RSA PRIVATE KEY-----
 
 1126;Social Job Sharing by Siemens;fr-FR;;learning_objects/B0D79010-868B-719B-28BF-BF39F8196A70.pptx
 1129;Tutoriel : Créer ses profils sur les réseaux sociaux LinkedIn et Twitter;fr-FR;;learning_objects/0B33937E-5348-271F-213A-8C26BA10EC70.pdf
